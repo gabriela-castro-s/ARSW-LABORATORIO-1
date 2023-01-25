62eea63555d4cf401146cdb4f29a3167e20b47c3
Carol Cely <caroltatianacely@gmail.com>
2023-01-25 09:22:56 -0500

    Parte 1 laboratorio

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -3,12 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.threads;
 
+package edu.eci.arsw.threads;
+import java.lang.Thread;
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread{
+    private int num1;
+    private int num2;
+    public CountThread(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public void run(){
+        System.out.println("thread is running...");
+        for(int i = num1+1; i < num2; i++){
+            System.out.println(i);
+        }
+    }
+
+
     
 }
